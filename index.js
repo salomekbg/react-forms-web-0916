4fc477a4199b3ea3ab4232ed7b2e083d3866d8d5
SluvsP <salomekbg@gmail.com>
2016-11-19 19:21:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-const app = "I don't do much.";
+const React = require('react');
+
+class ControlledInput extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      hi
+      // <input type="text" value={this.state.value} onChange={this.handleChange} />
+    )
+  }
+}
+
+module.exports = ControlledInput;
